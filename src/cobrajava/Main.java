a341df23b2cca25dca924a5453f44fe607180f32
vinny <144077568+v1s0or@users.noreply.github.com>
2025-04-19 05:46:11 -0500

    file upload
    blah!!!!

---
+++ src/cobrajava/Main.java
@@ -0,0 +1,103 @@
+package cobrajava;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 
+
+         .           ___/             
+ _   __ /|    ____ .'  /\   __.  .___ 
+ |   /   |   (     |  / | .'   \ /   \
+ `  /    |   `--.  |,'  | |    | |   '
+  \/    _|_ \___.' /`---'  `._.' /    
+                                      
+			Source Code
+	(i FINALLY know how to code in java)
+	quick v1s0or tip: if your building a port scanner
+	i recommend using "scanme.nmap.org" as your example to 
+	test it
+	
+	(I DONT SUPPORT ANY ILLEGAL ACTIVITES IN CYBERCRIME, I LIKE
+	DOING THESE PROJECTS FOR THE SILLYS AND BOREDOM)
+
+ */
+
+public class Main {
+	
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String PURPLE = "\u001B[35m";
+
+    public static void main(String[] args) {
+
+    	System.out.println(PURPLE + " _____       _               \r\n"
+    			+ "/  __ \\     | |              \r\n"
+    			+ "| /  \\/ ___ | |____   ____ _ \r\n"
+    			+ "| |    / _ \\| '_ \\ \\ / / _` |\r\n"
+    			+ "| \\__/\\ (_) | |_) \\ V / (_| |\r\n"
+    			+ " \\____/\\___/|_.__/ \\_/ \\__,_|                   \r\n"
+    			+ " Cobra Parody made in Java, might be slow.\r\n"
+    			+ " side project, spelt as CO + UV + A (B is slient)\r\n"
+    			+ " made by v1s0or\r\n"
+    			+ "" + RESET);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Do you want to scan a single port? (y/n)");
+        String answer = scanner.nextLine();
+        
+        while(true) {
+            if(answer.equals("n")) {
+            	main2(args);
+            	break; // will only run once
+            }
+            else {
+            	SinglePortScan.single(args); 
+            	break;
+            }
+        }
+    }
+    
+    private static void main2(String[] args) {
+    	Scanner scanner2 = new Scanner(System.in);
+        System.out.print("\nEnter host (e.g., localhost or 192.168.1.1): \n");
+        String host = scanner2.nextLine();
+
+        System.out.print("Enter starting port: ");
+        int startPort = scanner2.nextInt();
+
+        System.out.print("Enter ending port: ");
+        int endPort = scanner2.nextInt();
+
+        int timeout = 100; // milliseconds
+        int openPortCount = 0; // int to uhhh open ports
+        int closedPortCount = 0; // int to closed ports
+        List<Integer> openPorts = new ArrayList<>(); // list of the port numbers
+
+        System.out.println("\nScanning ports...\n");
+
+        for (int port = startPort; port <= endPort; port++) {
+            try (Socket socket = new Socket()) {
+                socket.connect(new InetSocketAddress(host, port), timeout);
+                System.out.println(GREEN + "[+] Port " + port + " is OPEN" + RESET);
+                openPorts.add(port); // counts the open port number
+                openPortCount++; // how many ports are opened are counted
+            } catch (IOException ex) {
+                System.out.println(RED + "[-] Port " + port + " is CLOSED" + RESET);
+                closedPortCount++;
+            }
+        }
+
+        System.out.println(PURPLE + "\nScanning complete." + RESET);
+        System.out.println("Open ports found: " + GREEN + openPortCount + RESET);
+        System.out.println(GREEN + openPorts + RESET);
+        System.out.println("\n");
+        System.out.println("Closed Ports: " + RED + closedPortCount + RESET);
+        scanner2.close();
+    }
+}
---
+++ src/cobrajava/SinglePortScan.java
@@ -0,0 +1,43 @@
+package cobrajava;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SinglePortScan {
+	
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    
+	public static void single(String[] args) {
+		Scanner scanner1 = new Scanner(System.in);
+		
+		System.out.println("\nEnter Host: ");
+		String host = scanner1.nextLine();
+		
+		System.out.println("Enter Port to scan");
+		int port = scanner1.nextInt();
+		
+		int timeout = 100;
+		
+		if(isPortOpen(host, port, timeout)) {
+			System.out.println("Port " + port + " is " + GREEN + "OPEN" + RESET);
+			}
+		else {
+			System.out.println("Port " + port + " is " + RED + "CLOSED" + RESET);
+		}
+		
+		}
+		
+	    public static boolean isPortOpen(String host, int port, int timeout) {
+	        try (Socket socket = new Socket()) {
+	            // Try to connect to the host and port within the given timeout
+	            socket.connect(new InetSocketAddress(host, port), timeout);
+	            return true; // If successful, the port is open
+	        } catch (IOException e) {
+	            return false; // If connection failed, the port is closed
+	    }
+	}
+}
